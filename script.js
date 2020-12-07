9a38e54f4c1c8f16b24248d391b591184c93b664
raizy21 <chiper_raizy@yahoo.com>
2020-12-07 11:40:44 +0100

    joke_teller_code_along

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+/******************
+ * Links
+ *
+ * {@link} - https://giphy.com/gifs/robot-cinema-4d-eyedesyn-3o7abtn7DuREEpsyWY
+ *          ROBOT.GIF from Giphy
+ * {@link} - https://rapidapi.com/voicerss/api/text-to-speech-1
+ * {@link} - https://rapidapi.com/
+ * {@link} - http://www.voicerss.org/api/
+ * {@link} - https://sv443.net/jokeapi/v2/
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ * {@link} - https://developers.google.com/web/fundamentals/primers/async-functions
+ * {@link} - https://www.w3schools.com/tags/ref_av_dom.asp
+ * {@link} - https://stackoverflow.com/questions/47822525/where-to-hide-an-api-key
+ ******************/
 
-// VoiceRSS Javascript SDK
-const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+
+
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+
+//disable/ enable button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+//passing joke to voice_rss api
+function tellMe(joke) {
+    //console.log('tell me: ', joke);
+
+    VoiceRSS.speech({
+        key: '08f6ae62021149ee8df0a87c6d9e76e9',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0,
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+
+}
+
+//get jokers from joke api
+async function getJokes() {
+    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+
+    try {
+        let joke = '';
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        // console.log(data.joke);
+        if (data.setup) {
+            //for twopart jokes
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        //console.log(joke);
+        //text-to-speech
+        tellMe(joke);
+        //disable button
+        toggleButton();
+    } catch (error) {
+        console.log('fetch failed ', error);
+    }
+}
+
+//event listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
